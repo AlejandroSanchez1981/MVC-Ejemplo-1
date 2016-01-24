48e87c7cf097b27526995653e2906ce7bf37a5cd
Ale <rodolfoalejandrosanchez@gmail.com>
2016-01-23 21:12:46 -0300

    prueba ..

---
+++ MVCEjemplo/MVCEjemplo/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCEjemplo.Models
+{
+    public class Cliente
+    {
+
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string  Celular { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public string Imagen { get; set; }
+
+
+
+    }
+
+}
---
+++ MVCEjemplo/MVCEjemplo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCEjemplo.Startup))]
+namespace MVCEjemplo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
